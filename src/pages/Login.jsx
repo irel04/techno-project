d21da0a5ef4fa1fbe6b374ea5c4c09a68a12f7f6
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-05-09 23:26:15 +0800

    feat: login authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { supabase } from '../utils/supabase'
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [data, setData] = useState(null)
 
-    const signIn = async () => {
-        const {data, error} = await supabase.auth.signInWithPassword({
-            email: email,
-            password: password
-        })
+    const signIn = async (e) => {
+        e.preventDefault()
+        try {
+            const {data: userData, error} = await supabase.auth.signInWithPassword({
+                email: email,
+                password: password
+            })
+            setData(userData)
+        } catch (error) {
+            console.error(error)
+        }
     }
 
 
-
     return (
-        <form className='rounded mt-20 container w-96 mx-auto bg-gray-300 flex flex-col py-14 items-center gap-8'>
+        <form onSubmit={signIn} className='rounded mt-20 container w-96 mx-auto bg-gray-300 flex flex-col py-14 items-center gap-8'>
             <div className='flex flex-col justify-center items-center gap-3'>
                 <h1 className='text-3xl font-bold'>Login</h1>
                 <h2>Welcome to JB Manyakol</h2>
@@ -24,11 +30,11 @@ const Login = () => {
             <div className='flex flex-col py-5 items-center gap-3'>
                 <div>
                     <label className='m-2' htmlFor="username">Username: </label>
-                    <input className='w-50' type="text" id='username' />
+                    <input className='w-50 px-3 py-0.5' type="text" id='username' onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div>
                     <label className='m-2' htmlFor="password">Password: </label>
-                    <input className='w-50' type="text" id='password' />
+                    <input className='w-50 px-3 py-0.5' type="password" id='password' onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div className='flex flex-col items-center'>
                     <button className='mt-8 mb-2 w-36 rounded text-white bg-neutral-800' type='submit'>Continue</button>
--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -4,6 +4,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-console.log(supabaseUrl, supabaseKey);
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
