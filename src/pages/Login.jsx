31c7222287860bfdd24479dfedf3207d536d053f
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-05-09 22:33:35 +0800

    feat: simple login ui using tailwind

---
+++ src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <form className='rounded mt-20 container w-96 mx-auto bg-gray-300 flex flex-col py-14 items-center gap-8'>
+            <div className='flex flex-col justify-center items-center gap-3'>
+                <h1 className='text-3xl font-bold'>Login</h1>
+                <h2>Welcome to JB Manyakol</h2>
+            </div>
+            <div className='flex flex-col py-5 items-center gap-3'>
+                <div>
+                    <label className='m-2' htmlFor="username">Username: </label>
+                    <input className='w-50' type="text" id='username' />
+                </div>
+                <div>
+                    <label className='m-2' htmlFor="password">Password: </label>
+                    <input className='w-50' type="text" id='password' />
+                </div>
+                <div className='flex flex-col items-center'>
+                    <button className='mt-8 mb-2 w-36 rounded text-white bg-neutral-800' type='submit'>Continue</button>
+                    <a className='text-sm'>Not yet registered? <span className='font-bold cursor-pointer'>Sign up here</span></a>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default Login
