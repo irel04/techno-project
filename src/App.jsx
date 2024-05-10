edaf0cc5fe9c9f80988fab0f1a0670bb840dc9af
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-05-10 10:59:54 +0800

    fix: changes Browser Router to Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
 const App = () => {
+  
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
