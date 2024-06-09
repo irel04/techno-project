fb39948ef146982c6e1d9ab16e0548787019b394
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-06-10 01:40:40 +0800

    feat: skeleton for loading details

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,16 +4,21 @@ import logo from "../assets/default.jpg";
 import * as yup from 'yup';
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabase";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { spStorageKey } from "../utils/constant";
+import CustomInputSkeleton from "../components/skeletons/CustomInputSkeleton";
 
 const schema = yup.object({
   firstName: yup.string().required().max(50).label("First Name"),
   lastName: yup.string().required().max(50).label("Last Name"),
-  email: yup.string().max(50).required().label("Email"),
   bday: yup.string().required().label("Birthday"),
   number: yup.string().max(12).label("Contact Number")
 })
 
 const passwordSchema = yup.object({
+  email: yup.string().max(50).required().label("Email"),
   password: yup.string().required().label("Password").max(50),
   newPassword: yup.string().required().label("New Password").max(51),
   confirmPassword: yup.string().required().label("Confirm Password").max(51),
@@ -22,7 +27,7 @@ const passwordSchema = yup.object({
 function Profile() {
 
   // Initialized useForm hook
-  const { register, formState: { errors }, reset, handleSubmit } = useForm({
+  const { register, formState: { errors }, reset, handleSubmit, setValue } = useForm({
     resolver: yupResolver(schema),
     mode: "onChange"
   })
@@ -30,6 +35,43 @@ function Profile() {
   const handleUpdateProfile = async () => {
     console.log("Hi Raven")
   }
+
+  // User id
+  const [user] = useLocalStorage(spStorageKey, null)
+
+  // states
+  const [isLoading, setIsLoading] = useState(false)
+
+  // Call details
+  useEffect(() => {
+    const fetchUserData = async() => {
+      setIsLoading(true)
+      const {id: credential_id} = user.user
+
+      try {
+        const {data: renters, error} = await supabase.from("renters").select("*").eq("credential_id", credential_id) .eq('is_active', true)
+        
+        if(error){
+          throw error
+        }
+
+        const {first_name, last_name, email, contact_number, birthday} = renters[0]
+
+        setValue("firstName", first_name)
+        setValue("lastName", last_name)
+        setValue("bday", birthday)
+        setValue("number", contact_number),
+        setValue("email", email)
+        setIsLoading(false)
+
+      } catch (error) {
+        console.error(error)
+      }
+
+    }
+
+    fetchUserData()
+  }, [])
 
 
   return (
@@ -51,55 +93,60 @@ function Profile() {
         <div className="w-full">
           <form onSubmit={handleSubmit(handleUpdateProfile)} className="rounded pt-0 p-5 flex flex-col gap-3">
             <h1 className="font-bold text-xl">Personal Information</h1>
+            {!isLoading ? (
+              <>
+                <Input
+                  label="First Name"
+                  placeholder=""
+                  register={register}
+                  name={"firstName"}
+                  error={errors.firstName}
+                  maxLength={51}
+                />
 
-            <Input
-              label="First Name"
-              placeholder=""
-              register={register}
-              name={"firstName"}
-              error={errors.firstName}
-              maxLength={51}
-            />
+                <Input
+                  label="Last Name"
+                  register={register}
+                  name={"lastName"}
+                  maxLength={51}
+                  error={errors.lastName}
+                />
 
-            <Input
-              label="Last Name"
-              register={register}
-              name={"lastName"}
-              maxLength={51}
-              error={errors.lastName}
-            />
+                <Input
+                  label="Email"
+                  type="email"
+                  register={register}
+                  maxLength={51}
+                  name={"email"}
+                  error={errors.email}
+                />
 
-            <Input
-              label="Email"
-              type="email"
-              register={register}
-              maxLength={51}
-              name={"email"}
-              error={errors.email}
-            />
+                <Input
+                  label="Birthday"
+                  type="date"
+                  register={register}
+                  name={"bday"}
+                  error={errors.bday}
+                />
 
-            <Input
-              label="Birthday"
-              type="date"
-              register={register}
-              name={"bday"}
-              error={errors.bday}
-            />
+                <Input
+                  label="Contact Number"
+                  type="number"
+                  register={register}
+                  maxLength={12}
+                  name={"number"}
+                />
 
-            <Input
-              label="Contact Number"
-              type="number"
-              register={register}
-              maxLength={12}
-              name={"number"}
-            />
-
-            <Button
-              className="w-fit px-5 ml-auto"
-              color="primary"
-            >
-              Save
-            </Button>
+                <Button
+                  className="w-fit px-5 ml-auto"
+                  color="primary"
+                >
+                  Save
+                </Button>
+              </>
+            ) : Array.from({length: 5}).map((_, index) => (
+              <CustomInputSkeleton/>
+            ))}
           </form>
           <ChangePassword/>
         </div>
@@ -111,7 +158,7 @@ function Profile() {
 const ChangePassword = () => {
 
   // Initialized useForm hook
-  const { register, formState: { errors }, reset, handleSubmit } = useForm({
+  const { register, formState: { errors }, reset, handleSubmit, set} = useForm({
     resolver: yupResolver(passwordSchema),
     mode: "onChange"
   })
@@ -120,11 +167,15 @@ const ChangePassword = () => {
     console.log("Password changed")
   }
 
+  useEffect(()=>{
+    
+  }, [])
+
   return (
     <form onSubmit={handleSubmit(handleChangePassword)} className="rounded pt-0 p-5 flex flex-col gap-3">
       <h1 className="font-bold text-xl">Security Information</h1>
 
-      <Input label="Current Password" error={errors.password} maxLength={51} register={register} name={"currentPassword"} readOnly={true} />
+      <Input label="Current Password" error={errors.password} maxLength={51} register={register} name={"password"} />
       <Input label="New Password" error={errors.newPassword} maxLength={51} register={register} name={"newPassword"} />
       <Input label="Confirm New Password" error={errors.confirmPassword} maxLength={51} register={register} name={"confirmPassword"} />
 
