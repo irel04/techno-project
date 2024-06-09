7c73f7a7eb3d659ee0afe640ea43f7216ff32db2
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-06-10 00:15:16 +0800

    feat: validation of form

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,37 @@
 import Button from "../components/Button";
 import Input from "../components/Input";
 import logo from "../assets/default.jpg";
+import * as yup from 'yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object({
+  firstName: yup.string().required().max(50).label("First Name"),
+  lastName: yup.string().required().max(50).label("Last Name"),
+  email: yup.string().max(50).required().label("Email"),
+  bday: yup.string().required().label("Birthday"),
+  number: yup.string().max(12).label("Contact Number")
+})
+
+const passwordSchema = yup.object({
+  password: yup.string().required().label("Password").max(50),
+  newPassword: yup.string().required().label("New Password").max(51),
+  confirmPassword: yup.string().required().label("Confirm Password").max(51),
+})
 
 function Profile() {
+
+  // Initialized useForm hook
+  const { register, formState: { errors }, reset, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange"
+  })
+
+  const handleUpdateProfile = async () => {
+    console.log("Hi Raven")
+  }
+
+
   return (
     <main className="flex flex-col lg:gap-[10rem] items-center justify-center my-[3rem] md:my-[5rem] lg:mx-[5%]">
       <section className="w-full gap-5 lg:gap-10 flex flex-col md:flex-row">
@@ -19,31 +48,50 @@ function Profile() {
           </Button>
         </div>
 
-        <div className="w-full ">
-          <form className="rounded pt-0 p-5 flex flex-col gap-3">
+        <div className="w-full">
+          <form onSubmit={handleSubmit(handleUpdateProfile)} className="rounded pt-0 p-5 flex flex-col gap-3">
             <h1 className="font-bold text-xl">Personal Information</h1>
 
             <Input
-              type="email"
-              label="Email"
-            />
-
-            <Input
-              type="name"
               label="First Name"
+              placeholder=""
+              register={register}
+              name={"firstName"}
+              error={errors.firstName}
+              maxLength={51}
             />
+
             <Input
-              type="name"
               label="Last Name"
-            />
-            <Input
-              type="number"
-              label="Contact Number"
+              register={register}
+              name={"lastName"}
+              maxLength={51}
+              error={errors.lastName}
             />
 
             <Input
-              type="date"
+              label="Email"
+              type="email"
+              register={register}
+              maxLength={51}
+              name={"email"}
+              error={errors.email}
+            />
+
+            <Input
               label="Birthday"
+              type="date"
+              register={register}
+              name={"bday"}
+              error={errors.bday}
+            />
+
+            <Input
+              label="Contact Number"
+              type="number"
+              register={register}
+              maxLength={12}
+              name={"number"}
             />
 
             <Button
@@ -53,25 +101,38 @@ function Profile() {
               Save
             </Button>
           </form>
-
-          <form className="rounded pt-0 p-5 flex flex-col gap-3">
-            <h1 className="font-bold text-xl">Security Information</h1>
-
-            <Input label="Current Password" />
-            <Input label="New Password" />
-            <Input label="Confirm New Password" />
-
-            <Button
-              className="w-fit px-5 ml-auto"
-              color="primary"
-            >
-              Save
-            </Button>
-          </form>
+          <ChangePassword/>
         </div>
       </section>
     </main>
   );
+}
+
+const ChangePassword = () => {
+
+  // Initialized useForm hook
+  const { register, formState: { errors }, reset, handleSubmit } = useForm({
+    resolver: yupResolver(passwordSchema),
+    mode: "onChange"
+  })
+
+  const handleChangePassword = async () => {
+    console.log("Password changed")
+  }
+
+  return (
+    <form onSubmit={handleSubmit(handleChangePassword)} className="rounded pt-0 p-5 flex flex-col gap-3">
+      <h1 className="font-bold text-xl">Security Information</h1>
+
+      <Input label="Current Password" error={errors.password} maxLength={51} register={register} name={"currentPassword"} readOnly={true} />
+      <Input label="New Password" error={errors.newPassword} maxLength={51} register={register} name={"newPassword"} />
+      <Input label="Confirm New Password" error={errors.confirmPassword} maxLength={51} register={register} name={"confirmPassword"} />
+
+      <Button className="w-fit px-5 ml-auto" color="primary">
+        Save
+      </Button>
+    </form>
+  )
 }
 
 export default Profile;
