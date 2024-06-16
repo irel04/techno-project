10b0e524382ef6d2ce3a2f69586e2ae9331523cf
jb <116430791+jbmacacua@users.noreply.github.com>
2024-06-16 20:09:51 +0800

    fix: fetching profile photo

--- src/components/RenterFeedback.jsx
+++ src/components/RenterFeedback.jsx
@@ -1,7 +1,9 @@
 import { FaStar } from "react-icons/fa";
 import { PROFILE_PHOTO } from "../utils/constant";
+import defaultImg from "../assets/default.jpg"; 
 
-function RenterFeedback({ img, name, feedback, link }) {
+
+function RenterFeedback({ img, name, feedback}) {
   const stars = Array(5)
     .fill(0)
     .map((_, index) => (  
@@ -14,7 +16,7 @@ function RenterFeedback({ img, name, feedback, link }) {
   return (
     <div className="w-full lg:w-[25rem] max-h-[20rem] flex flex-col items-center justify-center gap-4 bg-white shadow-custom p-4 rounded">
       <img
-        src={img ? `${PROFILE_PHOTO}covers/${img}` : defaultImg}
+        src={img ? `${PROFILE_PHOTO}${img}` : defaultImg}
         className="w-[5rem] h-[5rem] rounded-full"
       />
       <h1 className="text-2xl font-semibold">{name}</h1>
