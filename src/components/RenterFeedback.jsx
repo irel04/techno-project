dd31d343064d2abff27e818b5ee73f5442c6f0bc
Cris Julyan C. Olesco <92448053+crisjulyancolesco@users.noreply.github.com>
2024-06-17 17:21:00 +0800

    style(feedbacks):adjust container and positions

--- src/components/RenterFeedback.jsx
+++ src/components/RenterFeedback.jsx
@@ -13,14 +13,18 @@ function RenterFeedback({ img, name, feedback, rating}) {
   ));
 
   return (
-    <div className="w-full lg:w-[25rem] max-h-[20rem] flex flex-col items-center justify-center gap-4 bg-white shadow-custom p-4 rounded">
+    <div className="w-full lg:w-[20rem] h-[19rem] flex flex-col items-center justify-center gap-4 bg-white shadow-custom p-4 rounded">
       <img
         src={img ? `${PROFILE_PHOTO}${img}` : defaultImg}
         className="w-[5rem] h-[5rem] rounded-full"
       />
-      <h1 className="text-2xl font-semibold">{name}</h1>
-      <p className="text-center">{feedback}</p>
       <div className="flex gap-2"> {stars}</div>
+      <h1 className="text-2xl font-semibold">{name}</h1>
+      <div className="flex-1 overflow-hidden">
+        <div className="overflow-y-auto h-full px-2">
+          <p className="text-center">{feedback}</p>
+        </div>
+      </div>
     </div>
   );
 }
